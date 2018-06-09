b0a358e22d6750c4113695c2e9fe6477165b2f23
LilianKalkman <liliankalkman@hotmail.com>
2018-06-09 10:11:23 +0200

    parks and streets report

--- es6classes.js
+++ es6classes.js
@@ -15,6 +15,12 @@ class Town {
     this.name = name;
     this.buildyear = buildyear;
   }
+
+  calc(arr){
+    const sum = arr.reduce( (prev, cur) => prev + cur, 0);
+    return [sum, sum / arr.length]
+  }
+
 };
 
 class Park extends Town {
@@ -38,28 +44,13 @@ class Park extends Town {
 
   getNameTrees(){
     if(this.trees > 1000){
-      console.log(`${this.name} has more than 1000 trees`);
+      const string = `${this.name} has more than 1000 trees`;
+      return string;
+    } else {
+      return '';
     }
   }
-
 }
-
-
-
-// 3 parks in town
-const vondelpark = new Park('Vondelpark', 1888, 500, 10000);
-console.log(vondelpark);
-console.log(vondelpark.calculateTreeDensity());
-vondelpark.calculateAverageAge(1990, 8990, 2000);
-
-const rembrandtpark = new Park('Rembrandtpark', 1901, 800, 80000 );
-console.log(rembrandtpark);
-console.log(rembrandtpark.calculateTreeDensity());
-
-const sarphatipark = new Park('Sarphatipark', 2001, 80, 1000);
-console.log(sarphatipark);
-console.log(sarphatipark.calculateTreeDensity());
-
 
 
 class Street extends Town {
@@ -82,22 +73,54 @@ class Street extends Town {
     classification.set(3, 'normal');
     classification.set(3, 'big');
     classification.set(4, 'huge');
-    console.log(classification.get(this.size))
+    return classification.get(this.size);
+  }
 }
 
-}
+// 3 parks in town
+
+const vondelpark = new Park('Vondelpark', 1888, 1001, 10000);
+const rembrandtpark = new Park('Rembrandtpark', 1901, 800, 80000 );
+const sarphatipark = new Park('Sarphatipark', 2001, 80, 1000);
+const allParks = [];
+allParks.push(vondelpark, rembrandtpark, sarphatipark);
+console.log(allParks);
+
 
 // 4 streets in town
+
 const heemstedestraat = new Street('heemstedestraat', 1880, 2, 100);
-console.log(heemstedestraat);
 const kinkerstraat = new Street('kinkerstraat', 1889, 3, 150);
-console.log(kinkerstraat);
 const warmondstraat = new Street('warmondstraat', 1992, 2, 110);
-console.log(warmondstraat);
 const zeilstraat = new Street('zeilstraat', 1878, 1 , 80);
-console.log(zeilstraat);
+const allStreets = [];
+allStreets.push(heemstedestraat, kinkerstraat, warmondstraat, zeilstraat);
+console.log(allStreets);
 
 const [total, average] = zeilstraat.calculateTotalandAverageLength();
 console.log(total, average);
 
-console.log(zeilstraat.classify());
+const sizeZeil = zeilstraat.classify();
+console.log(sizeZeil);
+
+// function reportPark(park){
+//   console.log(`The ${park.name} park has a density of ${park.calculateTreeDensity()} and an Average
+//   age of ${park.calculateAverageAge()}. ${park.getNameTrees()}`);
+// };
+
+function reportAllParks(parks){
+  for(let key in parks){
+    console.log(`Park ${parks[key].name} has a tree dens of ${parks[key].calculateTreeDensity()} and an Average
+    age of ${parks[key].calculateAverageAge()}. ${parks[key].getNameTrees()} `);
+  }
+}
+
+function reportStreets(streets){
+  for(let key in streets){
+    console.log(`The ${streets[key].name} has a total of ${streets[key].calculateTotalandAverageLength()[0]} and a
+    average of ${streets[key].calculateTotalandAverageLength()[1]} and is of size: ${streets[key].classify()}`)
+  }
+};
+
+reportAllParks(allParks);
+reportStreets(allStreets);
